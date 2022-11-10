ba1825b8c02b679d169cf10b95d8e79bd7286122
Mo <ps.hackmaster@gmail.com>
2022-11-10 17:07:44 +0000

    bump version

--- routes/debug.js
+++ routes/debug.js
@@ -4,7 +4,7 @@ const { redisClient } = require("../utils/redis.js");
 
 const router = Router();
 
-router.get("/version", (req, res) => res.send("version 1.0"));
+router.get("/version", (req, res) => res.send("version 2.0"));
 
 router.get("/env", (req, res) => res.json(process.env));
 
